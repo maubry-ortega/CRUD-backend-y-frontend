64598c2e71b27a8c7d6b8b7c1bb1ba3cc0593bc4
maubry-ortega <ortegamaubry@gmail.com>
2025-11-30 01:57:53 +0000

    delete files

--- frontend/src/app/Services/Productos/producto.service.ts
+++ frontend/src/app/Services/Productos/producto.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, observable } from 'rxjs';
-import { environment } from 'environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -18,25 +18,25 @@ export class ProductoService {
 
   }
 
-  getProducto( id: number ): Observable<any[]> {
+  getProducto(id: number): Observable<any[]> {
 
     return this.httpClient.get<any>(`${this.baseUrl}productos/${id}`);
 
   }
 
-  createProducto( producto: any ): Observable<any> {
+  createProducto(producto: any): Observable<any> {
 
     return this.httpClient.post<any>(`${this.baseUrl}crearProducto`, producto);
 
   }
 
-  updateProducto( id: number, producto: any ): Observable<any> {
+  updateProducto(id: number, producto: any): Observable<any> {
 
     return this.httpClient.put<any>(`${this.baseUrl}productos/${id}`, producto);
 
   }
 
-  deleteProducto( id: number ): Observable<any> {
+  deleteProducto(id: number): Observable<any> {
     return this.httpClient.delete<any>(`${this.baseUrl}productos/${id}`);
   }
 }
